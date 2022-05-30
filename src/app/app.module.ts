35a4a34d94702f3e2c7e26f1eb30e378b7278e81
juan carlos penalba <jcpenalbaarnau@gmail.com>
2022-05-30 19:24:30 -0300

    configuracion inicial y creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardComponent } from './Components/card/card.component';
+import { SearchComponent } from './Components/search/search.component';
+import { DialogAlertComponent } from './Components/dialog-alert/dialog-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    SearchComponent,
+    DialogAlertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
